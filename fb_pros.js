bf7f1e3f6b0ac2687ee2330a084497a4070b582d
aadame3311 <antonio.adame01@utrgv.edu>
2017-11-05 02:29:53 -0600

    displays user profile information

--- fb_pros.js
+++ fb_pros.js
@@ -5,27 +5,29 @@ window.fbAsyncInit = function() {
     xfbml      : true,
     version    : 'v2.8'
   });
-    
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
 };
 
-(function(d, s, id){
 
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "https://connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=353634480666';
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-
-  
 
 function statusChangeCallback(response) {
  if (response.status === 'connected') {
       console.log("Logged in and Authenticated");
       setElements(true);
+      testAPI();
      
-   } else { 
+  } else { 
       console.log("not authenticated");    
       setElements(false);
       
@@ -33,13 +35,36 @@ function statusChangeCallback(response) {
    }
 }
 
+
+function testAPI() {
+  FB.api('/me?fields=name,email,birthday,location', function(response) {
+    if (response && !response.error) {
+      console.log(response);
+      userProfile(response);
+    }
+  });
+}
+
+function userProfile(user) {
+  let profile =`
+    <h1>${user.name}</h1>
+    <h3>Birthday: ${user.birthday}</h3>
+    <h3>Location: ${user.location.name}</h3>
+
+  `;
+
+  document.getElementById('info').innerHTML = profile;
+}
+
 function setElements(isLogged) {
     if (isLogged) {
         document.getElementById("fb-button").style.display = 'none';
         document.getElementById("logout-button").style.display = 'block';
+        document.getElementById("info").style.display = 'block';
     } else {
         document.getElementById("fb-button").style.display = 'block';
         document.getElementById("logout-button").style.display = 'none';
+        document.getElementById("info").style.display = 'none';
 
    }
 }
@@ -47,7 +72,6 @@ function logoutPressed(){
   FB.logout(function(response) {
     setElements(false);
   });
-  checkLoginState();
 }
 
 function checkLoginState() {
